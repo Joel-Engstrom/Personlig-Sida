96e65514fdb19d222bef183cb48e428547dcced1
Joel <mang.hansson@gmail.com>
2019-01-27 04:06:38 +0100

    Update på mina meriter

--- work.js
+++ work.js
@@ -1,8 +1,26 @@
 $(document).ready(function () {
-    //Skapa kontrollern.
-    var controller = new ScrollMagic.controller;
-    //Skapa första scenen.
-    var firstScene = new ScrollMagic.Scene({
-        triggerElement: "#"
-    });
+    //Texten i mitten.
+    $("#titleText").slideDown(1200);
+    $("#descText").delay(1200);
+    $("#descText").fadeIn();
+
+    //Nav Baren.
+    $("#navItem1").hide();
+    $("#navItem2").hide();
+    $("#navItem3").hide();
+    $("#navItem1").delay(2000).fadeIn(1000);
+    $("#navItem2").delay(2500).fadeIn(1000);
+    $("#navItem3").delay(3000).fadeIn(1000);
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0)
+        {
+            $(".Header").addClass("AfterScroll");
+            $(".NavUl").addClass("AfterScroll");
+        } else 
+        {
+            $(".Header").removeClass("AfterScroll");
+            $(".NavUl").removeClass("AfterScroll");
+        }
+    })
 });
